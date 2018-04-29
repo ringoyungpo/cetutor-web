1636409a4eb92763ac02ac1fb0e0d2a650866d47
Ringo Yungpo Kao <ringoyungpo@qq.com>
2018-04-29 16:12:57 +0800

    register and api conneted

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,35 +1,62 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default () => {
-  return (
-    <div class="login">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 m-auto">
-            <h1 class="display-4 text-center">Log In</h1>
-            <p class="lead text-center">Sign in to your CETutor account</p>
-            <form action="dashboard.html">
-              <div class="form-group">
-                <input
-                  type="email"
-                  class="form-control form-control-lg"
-                  placeholder="Email Address"
-                  name="email"
-                />
-              </div>
-              <div class="form-group">
-                <input
-                  type="password"
-                  class="form-control form-control-lg"
-                  placeholder="Password"
-                  name="password"
-                />
-              </div>
-              <input type="submit" class="btn btn-info btn-block mt-4" />
-            </form>
+export default class componentName extends Component {
+  constructor() {
+    super()
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  render() {
+    return (
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">
+                Sign in to your DevConnector account
+              </p>
+              <form onSubmit={this.onSubmit.bind(this)}>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChange.bind(this)}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange.bind(this)}
+                    required
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
--- client/src/components/auth/Ragister.js
+++ client/src/components/auth/Ragister.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import classnames from 'classnames'
 
 export default class Register extends Component {
   constructor() {
@@ -8,14 +10,29 @@ export default class Register extends Component {
       email: '',
       password: '',
       passwordComfirmed: '',
+      errors: {},
+    }
+  }
+
+  async onSubmit(e) {
+    e.preventDefault()
+    try {
+      const res = await axios.post('api/users/', this.state)
+      console.log(res.data)
+    } catch (error) {
+      this.setState({ errors: error.response.data })
+      console.log(error.response.data)
     }
   }
 
   onChange(e) {
+    this.setState({ errors: { ...this.state.errors, [e.target.name]: null } })
     this.setState({ [e.target.name]: e.target.value })
   }
 
   render() {
+    const { errors } = this.state
+
     return (
       <div className="register">
         <div className="container">
@@ -23,22 +40,29 @@ export default class Register extends Component {
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Sign Up</h1>
               <p className="lead text-center">Create your CETutor account</p>
-              <form action="create-profile.html">
+              <form onSubmit={this.onSubmit.bind(this)}>
                 <div className="form-group">
                   <input
                     type="text"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.nickname,
+                    })}
                     placeholder="Nickname"
                     name="nickname"
                     value={this.state.nickname}
                     onChange={this.onChange.bind(this)}
                     required
                   />
+                  {errors.nickname && (
+                    <div className="invalid-feedback">{errors.nickname}</div>
+                  )}
                 </div>
                 <div className="form-group">
                   <input
                     type="email"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.email,
+                    })}
                     placeholder="Email Address"
                     name="email"
                     value={this.state.email}
@@ -49,28 +73,44 @@ export default class Register extends Component {
                     This site uses Gravatar so if you want a profile image, use
                     a Gravatar email
                   </small> */}
+                  {errors.email && (
+                    <div className="invalid-feedback">{errors.email}</div>
+                  )}
                 </div>
+
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password,
+                    })}
                     placeholder="Password"
                     name="password"
                     value={this.state.password}
                     onChange={this.onChange.bind(this)}
                     required
                   />
+                  {errors.password && (
+                    <div className="invalid-feedback">{errors.password}</div>
+                  )}
                 </div>
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.passwordComfirmed,
+                    })}
                     placeholder="Confirm Password"
                     name="passwordComfirmed"
                     value={this.state.passwordComfirmed}
                     onChange={this.onChange.bind(this)}
                     required
                   />
+                  {errors.passwordComfirmed && (
+                    <div className="invalid-feedback">
+                      {errors.passwordComfirmed}
+                    </div>
+                  )}
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
