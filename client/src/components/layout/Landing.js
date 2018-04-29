47e850aeed309c039ddd74fa962a5fb0a0733623
Ringo Yungpo Kao <ringoyungpo@qq.com>
2018-04-29 13:43:45 +0800

    finish landing

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">CETutor</h1>
+                <p className="lead">
+                  {' '}
+                  A better way to prapare for Collage English Test
+                </p>
+                <hr />
+                <a href="register.html" className="btn btn-lg btn-info mr-2">
+                  Sign Up
+                </a>
+                <a href="login.html" className="btn btn-lg btn-light">
+                  Login
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
