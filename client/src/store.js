d56bade8e1c2555fd45a94b2dd6fa4156756565d
Ringo Yungpo Kao <ringoyungpo@qq.com>
2018-04-30 12:39:50 +0800

    fix chrome redux ext problem

--- client/src/store.js
+++ client/src/store.js
@@ -6,14 +6,15 @@ const initialState = {}
 
 const middleware = [thunk]
 
+const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__
+  ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  : null
+
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
+  applyMiddleware(...middleware),
+  compose(applyMiddleware(...middleware), reduxDevtools),
 )
 
 export default store
