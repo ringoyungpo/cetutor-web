704cfcc9b82462feffbd2854e886a58a8903232c
Ringo Yungpo Kao <ringoyungpo@qq.com>
2018-05-04 02:15:31 +0800

    fix keys_prod error again

--- client/src/config/keys_prod.js
+++ client/src/config/keys_prod.js
@@ -1,14 +1,7 @@
-module.exports = CLOUDINARY = {
+module.exports = {
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
   base: process.env.CLOUDINARY_BASE,
   audio: process.env.CLOUDINARY_AUDIO
-}
-module.exports = {
-  cloud_name: 'ringo',
-  api_key: '256925276693943',
-  api_secret: 'CGPeWS60km2JODcI4riHCCG4hGw',
-  base: 'https://res.cloudinary.com/ringo',
-  audio: 'https://api.cloudinary.com/v1_1/ringo/video/upload'
 }
